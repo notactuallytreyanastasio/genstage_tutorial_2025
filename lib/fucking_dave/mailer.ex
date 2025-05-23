dc0857eccd58e8cf281aead37c65cfb17e71050b
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-05-23 06:01:46 -0400

    remove it all for posting to GH

--- lib/fucking_dave/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule FuckingDave.Mailer do
-  use Swoosh.Mailer, otp_app: :fucking_dave
-end
--- lib/fucking_dave/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule FuckingDave.Repo do
-  use Ecto.Repo,
-    otp_app: :fucking_dave,
-    adapter: Ecto.Adapters.Postgres
-end
